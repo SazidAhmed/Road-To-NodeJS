dd84e7a1c72fb49466c3e09389996fec3bd644f3
SazidAhmed <sazidahmed.official@gmail.com>
2022-03-25 01:43:32 +0600

    crud completed

--- server/app/Controllers/ContactController.js
+++ server/app/Controllers/ContactController.js
@@ -12,29 +12,67 @@ const contactList = asyncHandler(
 
 const contactGetById = asyncHandler(
     async (req, res) => {
-        res.status(200).json({message:`get contacts ${req.params.id}`})
+        const contact = await Contact.findById(req.params.id)
+
+        if(!contact) {
+            res.status(400)
+            throw new Error('Contact not found')
+        }
+
+        res.status(200).json(contact)
     }
 )
 
 const contactCreate = asyncHandler(
     async (req, res) => {
-        if (!req.body.text) {
+        if (!req.body.name) {
             res.status(400)
-            throw new Error('Please add a text field')
-          }
-        res.status(200).json({message:`create contacts`})
+            throw new Error('Please add a name field')
+        }
+
+        const contact = await Contact.create({
+            name: req.body.name
+        })
+        
+        res.status(200).json({
+            message:'Contact created successfully',
+            contact: contact
+        })
     }
 )
 
 const contactUpdate = asyncHandler(
     async (req, res) => {
-        res.status(200).json({message:`get contacts ${req.params.id}`})
+        const contact = await Contact.findById(req.params.id)
+
+        if(!contact) {
+            res.status(400)
+            throw new Error('Contact not found')
+        }
+
+        const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, {new:true})
+
+        res.status(200).json({
+            message:'Contact updated successfully',
+            contact:updatedContact
+        })
     }
 )
 
 const contactDelete = asyncHandler(
     async (req, res) => {
-        res.status(200).json({message:`get contacts ${req.params.id}`})
+        const contact = await Contact.findById(req.params.id)
+
+        if(!contact) {
+            res.status(400)
+            throw new Error('Contact not found')
+        }
+        
+        await contact.remove()
+
+        res.status(200).json({
+            message:'Contact deleted successfully'
+        })
     }
 )
 
